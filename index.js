8c44659f59b9d771e112ba8b6f3a0682fde7ce3d
anighosal <121676026+anighosal@users.noreply.github.com>
2023-06-09 19:56:00 +0600

    forbidden added

--- index.js
+++ index.js
@@ -9,6 +9,26 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.send
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
+
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lhk2now.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -32,7 +52,7 @@ async function run() {
 
     app.post("/jwt", (req, res) => {
       const user = req.body;
-      const token = jwt.sign(user, env.process.ACCESS_TOKEN_SECRET, {
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
         expiresIn: "1h",
       });
       res.send({ token });
@@ -54,6 +74,13 @@ async function run() {
         return res.send({ message: "user already exists" });
       }
       const result = await usersCollection.insertOne(user);
+      res.send(result);
+    });
+
+    app.delete("/users/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await selectClassCollection.deleteOne(query);
       res.send(result);
     });
 
@@ -86,30 +113,36 @@ async function run() {
       res.send(result);
     });
 
-    app.get("/musicData", async (req, res) => {
-      const result = await musicDataCollection.find().toArray();
-      res.send(result);
-    });
+    // app.get("/musicData", async (req, res) => {
+    //   const result = await musicDataCollection.find().toArray();
+    //   res.send(result);
+    // });
 
     // popular 6 class sort
     app.get("/musicData", async (req, res) => {
       const sort = req.query.sort;
 
       // const query = {};
-      const query = { numberOfStudents: { $gte: 11, $lte: 16 } };
+      // const query = { numberOfStudents: { $gte: 11, $lte: 16 } };
       const options = {
         sort: { numberOfStudents: asc === "asc" ? 1 : -1 },
       };
-      const result = await musicDataCollection.find(query, options).toArray();
+      const result = await musicDataCollection.find({}).limit(6).toArray();
       res.send(result);
     });
 
     // class collection apis
-    app.get("/classes", async (req, res) => {
+    app.get("/classes", verifyJWT, async (req, res) => {
       const email = req.query.email;
       console.log(email);
       if (!email) {
         res.send([]);
+      }
+      const decodedEmail = req.decoded.email;
+      if (email !== decodedEmail) {
+        return res
+          .status(403)
+          .send({ error: true, message: "forbidden access" });
       }
       const query = { email: email };
       const result = await selectClassCollection.find(query).toArray();
@@ -128,7 +161,7 @@ async function run() {
       const result = await selectClassCollection.deleteOne(query);
       res.send(result);
     });
-    // Send a ping to confirm a successful connection
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
